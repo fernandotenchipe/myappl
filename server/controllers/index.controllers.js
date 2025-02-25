76180d4e9248d7af2a18838292fb398092267342
Fernando Tenchipe Morales <fer.tenchipe@tec.mx>
2025-02-24 19:47:11 -0600

    inicio de backend

---
+++ server/controllers/index.controllers.js
@@ -0,0 +1,3 @@
+
+export const getIndex = (req, res) =>res.send('Hello World from API!');
+export const getPing = (req, res) => res.send('Pong!');
---
+++ server/index.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+import express from 'express';
+import indexRoutes from './routes/index.routes.js';
+
+const app = express();
+
+app.use(indexRoutes);
+
+app.listen(5000, console.log('http://localhost:5000'));
+
